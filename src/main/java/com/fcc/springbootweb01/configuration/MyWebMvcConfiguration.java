d4dafbff8348536288a842818a3b6fc9efbbcd6c
test2 <test2@163.com>
2022-08-14 11:56:52 +0800

    idea commit！

---
+++ src/main/java/com/fcc/springbootweb01/configuration/MyWebMvcConfiguration.java
@@ -0,0 +1,23 @@
+package com.fcc.springbootweb01.configuration;
+
+import com.fcc.springbootweb01.util.MyIntercepter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.context.request.WebRequestInterceptor;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+//@Configuration
+public class MyWebMvcConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new MyIntercepter())
+                .addPathPatterns("/**")
+                .excludePathPatterns("/", "/login")
+                .excludePathPatterns("/css/**", "/fonts/**", "/images/**", "/js/**");
+
+    }
+}
+    
+
